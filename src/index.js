5b72aa5b8c55de95db118b79a9214479c34b2784
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-18 10:52:51 +1000

    feat: router scaffolds and homepage wip

--- src/index.js
+++ src/index.js
@@ -4,14 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BlogProvider } from './contexts/BlogContext';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EntryForm from './components/EntryForm';
+import EntryParent from './components/EntryParent';
+import { Template } from './pages/_template';
+import { HomePage } from './pages/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BlogProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          {/* Template shows navbar and footer */}
+          <Route path='/' element={<Template />}>
+            {/* Shows ALL posts as read-only */}
+            <Route index element={<HomePage />} />
+            {/* Show individual post with edit toggle */}
+            {/* <Route path='/view/:id' element={<EntryPage />} />
+            {/* Show a form to make a new post  */}
+            {/* <Route path='/create' element={<NewEntryPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </BlogProvider>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
